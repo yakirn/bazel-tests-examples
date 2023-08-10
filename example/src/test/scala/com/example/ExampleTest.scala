31cca5352f74d88dd9da872218b3785bffbc1170
yakirn <yakirn@wix.com>
2023-08-10 11:25:23 +0300

    cleanup

--- example/src/test/scala/com/example/ExampleTest.scala
+++ example/src/test/scala/com/example/ExampleTest.scala
@@ -2,14 +2,13 @@ package com.example
 
 import com.exampale.Calculator
 import org.specs2.concurrent.ExecutionEnv
-import org.specs2.specification.core.AsExecution
 import org.specs2.mutable.SpecificationWithJUnit
 
 class ExampleTest(implicit ee: ExecutionEnv) extends SpecificationWithJUnit {
   "running me works" should {
     "running only me fails" should {
       "cant find me" >> {
-        success
+        Calculator.add(1, 2) mustEqual 3
       }
     }
   }
--- test-utils/util/Assertions.scala
+++
@@ -1,8 +0,0 @@
-package util
-
-
-object Assertions {
-    def assertLength[A](a: List[A], expected: Int): Unit = {
-        assert(a.length == expected)
-    }
-}
